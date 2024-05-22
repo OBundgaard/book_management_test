8b7ae8805f647857ee5fc2ed8b66b4f10b14c75f
Oliver <oliverbundgaard@live.dk>
2024-05-22 14:19:21 +0200

    Added data driven unit tests

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 
 
 class Book:
-    def __init__(self, book_id, title, author, published_date):
+    def __init__(self, book_id: int, title: str, author: str, published_date: str | datetime):
         self.book_id = book_id
         self.title = title
         self.author = author
--- tests/test_app_data_driven.py
+++ tests/test_app_data_driven.py
@@ -1,24 +1,73 @@
+import json
 import unittest
 from models import Book
+from app import app
+import ddt
 
 
-class TestBookCreation(unittest.TestCase):
-    def test_book_creation(self):
-        test_cases = [
-            (1, 'Test Book 1', 'Author 1', '2022-01-01'),
-            (2, 'Test Book 2', 'Author 2', '2023-01-01'),
-            (3, 'Test Book 3', 'Author 3', '2024-01-01'),
-            (4, 'Test Book 34', 'Author 3', '2024-01-01'),
-            (5, 'Test Book 36', 'Author 3', '2024-01-01'),
-            (6, 'Test Book 312', 'Author 3', '2024-01-01'),
-        ]
-        for book_id, title, author, published_date in test_cases:
-            with self.subTest(book_id=book_id, title=title, author=author, published_date=published_date):
-                book = Book(book_id=book_id, title=title, author=author, published_date=published_date)
-                self.assertEqual(book.book_id, book_id)
-                self.assertEqual(book.title, title)
-                self.assertEqual(book.author, author)
-                self.assertEqual(book.published_date, published_date)
+@ddt.ddt
+class DataTestApp(unittest.TestCase):
+    def setUp(self):
+        # Setting up the testing itself
+        self.app = app.test_client()
+        self.app.testing = True
+
+    @ddt.data(
+        Book(book_id=2, title='Sample book', author='Sample author', published_date='1970-1-1').to_dict(),
+        Book(book_id=4, title='Sample book', author='Sample author', published_date='1970-1-1').to_dict(),
+        Book(book_id=5, title='Sample book', author='Sample author', published_date='1970-1-1').to_dict(),
+    )
+    def test_post_books(self, case):
+        # Arrange
+        data = json.dumps(case)
+
+        # Act
+        response = self.app.post('/books', data=data, content_type='application/json')
+
+        # Assert
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json['title'], case['title'])
+
+    @ddt.data(3, 4, 9)
+    def test_get_books(self, case):
+        # Arrange
+        book_id = case
+
+        # Act
+        response = self.app.get(f'/books/{book_id}')
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+
+    @ddt.data(
+        Book(book_id=1, title='Sample book', author='A python developer', published_date='1970-1-1'),
+        Book(book_id=4, title='Sample book', author='A python developer', published_date='1970-1-1'),
+        Book(book_id=5, title='Sample book', author='A python developer', published_date='1970-1-1'),
+    )
+    def test_put_books(self, case):
+        # Arrange
+        updated_book = case.to_dict()
+        data = json.dumps(updated_book)
+        book_id = case.book_id
+
+        # Act
+        response = self.app.put(f'/books/{book_id}', data=data, content_type='application/json')
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['author'], 'A python developer')
+
+    @ddt.data(1, 2, 9)
+    def test_delete_books(self, case):
+        # Arrange
+        book_id = case
+
+        # Act
+        response = self.app.delete(f'/books/{book_id}')
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['message'], 'Book deleted')
 
 
 if __name__ == '__main__':
